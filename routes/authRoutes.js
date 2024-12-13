3a2e53361222a9a43bc8081a31416715a41c44f1
gienicka <mgazotillo@gbox.ncf.edu.ph>
2024-12-13 23:52:27 +0800

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { register, login, deleteUser, getAllUsers } = require('../controllers/authController'); // Include getAllUsers
+const { register, login, deleteUser, getAllUsers, updateUser } = require('../controllers/authController'); // Include updateUser
 const router = express.Router();
 
+// Existing routes
 router.post('/register', register);
 router.post('/login', login);
 router.delete('/delete', deleteUser);
 router.get('/users', getAllUsers); // Route for getting all users
+
+// New route for updating user information
+router.post('/update', updateUser); // POST method for updating a user
 
 module.exports = router;
